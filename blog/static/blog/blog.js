c2fe9be0e19b46f3273a5922c0f898ab4ba8b6b7
Lakshay Singh <lakshay18singh@gmail.com>
2023-08-15 20:44:02 +0000

    Finish Javascripts functions and loops

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,14 @@
-const theNumber = 1
-//let yourName = 'Ben'
-
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  alert(yourName)
+for(let i = 0; i < 10; i += 1) {
+  console.log("for loop : " + i)
 }
+i = 0
+while(i < 10) {
+  console.log("while loop " + i)
+  i++
+}
+const numbers = [1, 2,3 ,4 ,5, 6, 7, 8, 9]
 
-alert(yourName)
+numbers.forEach(x => console.log("For Each: " + x))
+
+const doubled = numbers.map(x => x*2)
+console.log(doubled)
